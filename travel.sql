f90cb2a237318feaec2c33634966b57e6cf5e77b
rezanandika <rezanandika30@gmail.com>
2019-11-29 10:10:49 +0700

    pergantian sql di pemesanan

--- travel.sql
+++ travel.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 26 Nov 2019 pada 09.45
+-- Generation Time: 29 Nov 2019 pada 04.10
 -- Versi Server: 10.1.19-MariaDB
 -- PHP Version: 5.6.28
 
@@ -73,15 +73,6 @@ CREATE TABLE `jadwal_berangkat` (
   `waktu_order` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data untuk tabel `jadwal_berangkat`
---
-
-INSERT INTO `jadwal_berangkat` (`id_jadwal`, `kode_jadwal`, `tanggal_berangkat`, `jam_berangkat`, `id_pemesanan`, `id_pengemudi`, `id_kota_asal`, `id_kota_tujuan`, `jumlah_pemesanan`, `status`, `waktu_order`) VALUES
-(4, 1, '2019-11-12', '16:05:00', '19PSN000', 3, 1, 2, 1, '3', '0000-00-00 00:00:00'),
-(5, 1, '2019-11-12', '06:00:00', '19PSN001', 3, 1, 2, 1, '3', '0000-00-00 00:00:00'),
-(6, 2, '2019-11-12', '07:15:00', '19PSN002', 6, 1, 2, 2, '1', '0000-00-00 00:00:00');
-
 -- --------------------------------------------------------
 
 --
@@ -130,7 +121,7 @@ CREATE TABLE `pemesanan` (
   `id_pemesanan` char(8) NOT NULL,
   `kode_jadwal` int(11) NOT NULL,
   `nama_pemesan` varchar(50) NOT NULL,
-  `telp_pemesan` varchar(13) NOT NULL,
+  `telp_pemesan` varchar(14) NOT NULL,
   `jumlah_pemesanan` int(11) NOT NULL,
   `tanggal_berangkat` date NOT NULL,
   `jam_berangkat` time NOT NULL,
@@ -142,16 +133,6 @@ CREATE TABLE `pemesanan` (
   `status` char(2) NOT NULL,
   `waktu_order` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `pemesanan`
---
-
-INSERT INTO `pemesanan` (`id_pemesanan`, `kode_jadwal`, `nama_pemesan`, `telp_pemesan`, `jumlah_pemesanan`, `tanggal_berangkat`, `jam_berangkat`, `id_kota_asal`, `id_kota_tujuan`, `lokasi_penjemputan`, `harga`, `id_pengemudi`, `status`, `waktu_order`) VALUES
-('19PSN000', 0, 'reza', '082329672695', 1, '2019-11-12', '16:05:00', 1, 2, 'disini aja', 0, 3, '3', '2019-11-21 16:07:11'),
-('19PSN001', 0, 'dika', '123435', 1, '2019-11-12', '06:00:00', 1, 2, 'dimana aja', 0, 3, '3', '0000-00-00 00:00:00'),
-('19PSN002', 0, 'didi', '082326594233', 2, '2019-11-12', '07:15:00', 1, 2, 'dimana aja\r\n', 0, 6, '1', '2019-11-25 19:29:36'),
-('19PSN003', 0, 'ebet', '123456789', 1, '2019-11-12', '12:10:00', 2, 1, 'yoi', 0, 0, '5', '2019-11-25 22:09:05');
 
 -- --------------------------------------------------------
 
